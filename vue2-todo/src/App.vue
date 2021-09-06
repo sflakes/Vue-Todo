7999134f8cc0076e80d3c489b70bd01b7cc5346a
rury fatkhuri fazri <fatkhurifazri@gmail.com>
2021-09-07 05:02:59 +0700

    update

--- vue2-todo/src/App.vue
+++ vue2-todo/src/App.vue
@@ -1,35 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Todo/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Todo />
   </div>
 </template>
 
 <script>
-import Todo from './components/Todo.vue'
+import Todo from "./components/Todo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Todo,
-  }
-}
+  },
+};
 </script>
 
 <style>
-*{
-   font-family: Avenir, Helvetica, Arial, sans-serif;
+* {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   margin: 0;
   padding: 0;
   text-align: center;
-  
 }
 #app {
-  background: #1D2B64;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to left, #F8CDDA, #1D2B64);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to left, #F8CDDA, #1D2B64); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: #1f1c2c; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to left,
+    #928dab,
+    #1f1c2c
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to left,
+    #928dab,
+    #1f1c2c
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
   background-repeat: no-repeat;
   height: 100vh;
